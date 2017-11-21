5887b5fb0360760a068e9c3cbd8feb48996c38ae
Gao, Xiang <qasdfgtyuiop@gmail.com>
2017-11-21 00:10:37 -0500

    unblock kernel.org

--- select-mirrors.sh
+++ select-mirrors.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-grep -v kernel /etc/pacman.d/mirrorlist > /etc/pacman.d/mirrorlist.full # kernel.org mirror seems not syncing well
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.full
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.full > /etc/pacman.d/mirrorlist
